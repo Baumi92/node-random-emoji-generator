05700196ba7c557e3c75c576a8a1f97185a1b862
Manuel Baumgartner <manu.baumi@outlook.com>
2023-04-25 15:43:23 +0200

    Add first working implementation

--- README.md
+++ README.md
@@ -2,6 +2,25 @@
 
 Generate random emojis on the command line
 
+## Random Emoji
+
 ```bash
-TODO: add an output for the program
+$ node index.js
+❤️
+$ node index.js
+🍎
+```
+
+## Emoji by name
+
+```bash
+$ node index.js strawberry
+🍓
+```
+
+Invalid emoji names will print an error
+
+```bash
+$ node index.js strawberryyy
+Invalid emoji name
 ```
--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log('Hello');
+import emoji from 'node-emoji';
+
+const randomEmoji = emoji.random();
+
+if (!emojiName) {
+  const randomEmoji = emoji.random();
+  console.log(randomEmoji.emoji);
+} else if (foundEmoji) {
+  const foundEmoji = emoji.find(emojiName);
+  if (foundEmoji) {
+    console.log(foundEmoji.emoji);
+  } else {
+    console.log('Invalid emoji name');
+  }
+}
